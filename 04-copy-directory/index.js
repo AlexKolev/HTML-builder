e5153b88695db5c211dafb2780510053cd96b3dd
Defry <defryalex@gmail.com>
2022-05-14 18:39:39 +0500

    feat: make 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,29 @@
 
 const fs = require('fs');
-
 const path = require('path');
 
 const sourceFolder = path.join(__dirname, 'files');
 const copyFolder = path.join(__dirname, 'files-copy');
 
-fs.mkdir('/tmp/a/apple', { recursive: true }, (err) => {
-	if (err) throw err;
+
+fs.access(copyFolder, fs.constants.F_OK, (err) => {
+	if (err) {
+		fs.mkdir(copyFolder, (err) => {
+			if (err) throw err;
+		});
+	}
 });
+
+fs.readdir(sourceFolder, (err, files) => {
+	files.forEach(file => {
+		fs.stat(path.join(sourceFolder, file), (err, stats) => {
+			if (!stats.isDirectory()) {
+				fs.copyFile(path.join(sourceFolder, file), path.join(copyFolder, file), (err) => {
+					if (err) throw err;
+				});
+			}
+		});
+	});
+});
+
+
